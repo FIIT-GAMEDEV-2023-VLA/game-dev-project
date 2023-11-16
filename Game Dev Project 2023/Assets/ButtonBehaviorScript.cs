a5f464f7bdf5aeede888fe10fe5763d84450177e
arisucchi <43641550+arisucchi@users.noreply.github.com>
2023-11-16 19:05:42 +0100

    some changes unresolved so with some errors

--- Game Dev Project 2023/Assets/ButtonBehaviorScript.cs
+++ Game Dev Project 2023/Assets/ButtonBehaviorScript.cs
@@ -1,33 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenuScript : MonoBehaviour
+public class ButtonBehaviorScript : MonoBehaviour
 {
-
     public Text buttonNGText; // 'cause I want to change color of text in button when selected/hovered (to obtain responsiveness)
-    //public string RGBAColor;
     public Color colorClicked;
     
-    void Update()
+    public void ColorClickedButton()
     {
         
         
     }
-
-
-    public void NewGame()
-    {
-        ChangeColor(colorClicked);
-        
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-
-
-
+    
     public void ChangeColor(Color color)
     {
         
