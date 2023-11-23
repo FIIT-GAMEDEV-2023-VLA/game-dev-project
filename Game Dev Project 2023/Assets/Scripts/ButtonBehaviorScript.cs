e72714aa2f2defa7b071346dc8abbf70671f971e
arisucchi <43641550+arisucchi@users.noreply.github.com>
2023-11-23 19:04:21 +0100

    changing som stuff in progress

--- Game Dev Project 2023/Assets/Scripts/ButtonBehaviorScript.cs
+++ Game Dev Project 2023/Assets/Scripts/ButtonBehaviorScript.cs
@@ -11,33 +11,71 @@ public class ButtonBehaviorScript : MonoBehaviour
 {
     // 'cause I want to change color of text in button when selected/hovered (to obtain responsiveness)
     // this script is just for setting colors
-    public Color colorClicked;
-    public Color colorUnclicked;
-    public Color colorHovered;
-    public Color colorSelected;
-    public Color colorHoveredSelected;
     
-    public Text buttonText;
+    //public Color colorClicked;
+    //public Color colorUnclicked;
+    //public Color colorHovered;
+    //public Color colorSelected;
+    //public Color colorHoveredSelected;
     
-    
-    
-    
-    
-    
-    
-    
-    public Image[] imageClicked;
-    public Image[] imageUnclicked;
-    public Image[] imageHovered;
-    public Image[] imageSelected;
-    public Image[] imageHoveredSelected;
+    //public Text buttonText;
     
     
     
     
     
     
-    public Image buttonImage;
+    
+    
+    
+    public Button[] buttons;
+    public Sprite[] imageClicked;
+    public Sprite[] imageUnclicked;
+    public Sprite[] imageHovered;
+    public Sprite[] imageSelected;
+    public Sprite[] imageHoveredSelected;
+    
+    
+    
+    
+    
+    
+    
+    
+    public Text ChangeOfColorClickedButtonText(Text actualButtonText)  // color of text changes when button is clicked
+    {
+        buttonText = actualButtonText;
+        buttonText.color = colorClicked;
+        return buttonText;
+    }
+    
+    public Text ChangeOfColorUnclickedButtonText(Text actualButtonText)
+    {
+        buttonText = actualButtonText;
+        buttonText.color = colorUnclicked;
+        return buttonText;
+    }
+    
+    public Text ChangeOfColorHoveredButtonText(Text actualButtonText)
+    {
+        buttonText = actualButtonText;
+        buttonText.color = colorHovered;
+        return buttonText;
+    }
+    
+    public Text ChangeOfColorSelectedButtonText(Text actualButtonText)  // selected buttons will have different color
+    {
+        buttonText = actualButtonText;
+        buttonText.color = colorSelected;
+        return buttonText;
+    }
+    
+    public Text ChangeOfColorHoveredSelectedButtonText(Text actualButtonText)  // also hovering on selected button will look different
+    {
+        buttonText = actualButtonText;
+        buttonText.color = colorHoveredSelected;
+        return buttonText;
+    }
     
     
     
