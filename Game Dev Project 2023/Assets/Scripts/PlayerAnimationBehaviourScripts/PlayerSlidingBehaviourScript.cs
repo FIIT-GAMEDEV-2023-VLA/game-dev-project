600ab60d41fb96a1a51188d79cd2700e6b1bcbed
Dewflio <43574262+Dewflio@users.noreply.github.com>
2023-11-30 13:28:08 +0100

    Sliding Box Collider

--- Game Dev Project 2023/Assets/Scripts/PlayerAnimationBehaviourScripts/PlayerSlidingBehaviourScript.cs
+++ Game Dev Project 2023/Assets/Scripts/PlayerAnimationBehaviourScripts/PlayerSlidingBehaviourScript.cs
@@ -9,17 +9,28 @@ public class PlayerSlidingBehaviourScript : StateMachineBehaviour
     
 
     private GameObject player;
-    private PlayerScript playerScript; 
+    private PlayerScript playerScript;
+    private BoxCollider2D playerBoxCollider2D;
+
+    private float boxColliderEnterSizeY;
+    private float boxColliderEnterOffsetY;
     public void OnEnable()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         playerScript = player.GetComponent<PlayerScript>();
+        playerBoxCollider2D = player.GetComponent<BoxCollider2D>();
     }
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        boxColliderEnterSizeY = playerBoxCollider2D.size.y;
+        boxColliderEnterOffsetY = playerBoxCollider2D.offset.y;
 
+        float newSizeY = playerBoxCollider2D.size.y / 2;
+
+        playerBoxCollider2D.size = new Vector2(playerBoxCollider2D.size.x, newSizeY);
+        playerBoxCollider2D.offset = new Vector2(playerBoxCollider2D.offset.x, playerBoxCollider2D.offset.y - (newSizeY / 2));
         playerScript.LockInput();
     }
     
@@ -27,6 +38,8 @@ public class PlayerSlidingBehaviourScript : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        playerBoxCollider2D.size = new Vector2(playerBoxCollider2D.size.x, boxColliderEnterSizeY);
+        playerBoxCollider2D.offset = new Vector2(playerBoxCollider2D.offset.x, boxColliderEnterOffsetY);
         playerScript.UnlockInput();
     }
     
